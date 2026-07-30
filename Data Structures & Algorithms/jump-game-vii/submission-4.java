547aa0d6b29561d46911989e8490524b9c08b9e8
txcallen <allenxietianchong@gmail.com>
2026-07-30 10:34:34 -0400

    Add: jump-game-vii - submission-4

---
+++ Data Structures & Algorithms/jump-game-vii/submission-4.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean canReach(String s, int minJump, int maxJump) {
+        
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(0);
+
+        if(s.charAt(s.length()-1) == '1'){
+            return false;
+        }
+
+        int far = 0;
+
+        while(!que.isEmpty()){
+            int cur = que.poll();
+
+            int start = Math.max(cur+minJump, far + 1);
+
+            for(int j = start; j <= Math.min(cur + maxJump, s.length()-1); j++){
+                if(s.charAt(j) == '0'){
+                    que.offer(j);
+                    if(j == s.length()-1){
+                        return true;
+                    }
+                }
+            }
+
+            far = cur + maxJump;
+        }
+
+        return false;
+    }
+}
